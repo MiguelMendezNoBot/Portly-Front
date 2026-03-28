b73d10b387b38ba4d2f2a338115426c8ac1aaea8
Elsebas7 <sebastianlakkarivera7@gmail.com>
2026-03-28 16:53:19 -0400

    refactor: limpiar comentarios innecesarios en main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-// Importamos la página de Login que acabamos de crear
 import { LoginPage } from './pages/LoginPage';
 
 const rootElement = document.getElementById('root');
 if (rootElement) {
   createRoot(rootElement).render(
     <StrictMode>
-      {/* Mostramos el Login */}
+      {}
       <LoginPage />
     </StrictMode>
   );
