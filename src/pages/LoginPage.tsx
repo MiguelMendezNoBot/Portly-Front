e2fea9bca4aa97504c07474fd4b0f413b1de727b
Mayra <mayvillarroel2005@gmail.com>
2026-03-28 21:39:48 -0400

    Fix: change name of PestanaEsquina

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,12 +1,11 @@
-import PestanaEsquina from "../components/PestanaEsquina"
+import CornerTab from "../components/CornerTab"
 import {LoginForm} from "../features/auth/components/LoginForm"
 export const LoginPage = () => {
     return (
         <div className="bg-white p-3 min-h-screen">
             <div className="bg-[#0d152b] rounded-[20px] min-h-[calc(100vh-25px)] flex items-center justify-center py-6">
-                {/* <RegisterForm /> */}
                 <LoginForm/>
-                <PestanaEsquina />
+                <CornerTab />
             </div>
         </div>
     )
--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -1,11 +1,11 @@
-import PestanaEsquina from "../components/PestanaEsquina"
+import CornerTab from "../components/CornerTab"
 import { RegisterForm } from "../features/auth/components/RegisterForm"
 export const RegisterPage = () => {
     return (
         <div className="bg-white p-3 min-h-screen">
             <div className="bg-[#0d152b] rounded-[20px] min-h-[calc(100vh-25px)] flex items-center justify-center py-6">
                 <RegisterForm />
-                <PestanaEsquina />
+                <CornerTab />
             </div>
         </div>
     )
