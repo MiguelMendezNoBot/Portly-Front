6497aa0ad5291297f1cd619b0e8d42559f4d9928
CrhistoferVera <cristofervera110@gmail.com>
2026-04-04 21:23:54 -0400

    change scroll color

--- src/modules/home/presentation/pages/HomePage.tsx
+++ src/modules/home/presentation/pages/HomePage.tsx
@@ -11,7 +11,7 @@ export const HomePage = () => {
           <UserTab />
         </PestanaEsquina>
 
-        <div className="flex-1 overflow-y-auto overflow-x-hidden pt-20 sm:pt-0">
+        <div className="flex-1 overflow-y-auto overflow-x-hidden pt-20 sm:pt-0 scrollbar-thin">
           <Navbar />
           <HeroSection />
         </div>
