ac3c91077b59a433334e538ef116b140bfd376f9
CrhistoferVera <cristofervera110@gmail.com>
2026-04-05 19:51:59 -0400

    add eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -4,32 +4,55 @@ import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import prettier from 'eslint-plugin-prettier';
 import eslintConfigPrettier from 'eslint-config-prettier/flat';
+// 1. Importa el paquete de typescript-eslint
+import tseslint from 'typescript-eslint';
 
-export default [
+export default tseslint.config(
   { ignores: ['dist'] },
 
   js.configs.recommended,
-  reactHooks.configs.flat.recommended,
-  reactRefresh.configs.vite,
-  eslintConfigPrettier,
+  // 2. Carga las configuraciones recomendadas de TS
+  ...tseslint.configs.recommended,
 
   {
-    files: ['**/*.{js,jsx}'],
+    // 3. Extiende los archivos permitidos a TS y TSX
+    files: ['**/*.{js,jsx,ts,tsx}'],
     plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
       prettier,
     },
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      // 4. Usa el parser de TypeScript para que entienda tu código
+      parser: tseslint.parser,
       parserOptions: {
-        ecmaVersion: 'latest',
         ecmaFeatures: { jsx: true },
-        sourceType: 'module',
       },
     },
     rules: {
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
-      'prettier/prettier': 'error',
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      // 5. Regla para variables no usadas (ignora las que empiezan con _)
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_',
+        },
+      ],
+      'prettier/prettier': [
+        'error',
+        {
+          endOfLine: 'auto', // Esto evita los errores de Delete ␍ que tenías
+        },
+      ],
     },
   },
-];
+  eslintConfigPrettier // Siempre al final para que mande sobre el resto
+);
