04c5c627f1dc396376d8f9e65554580744a4db72
Mayra <mayvillarroel2005@gmail.com>
2026-04-05 21:06:48 -0400

    fix: Change lenguage of home page

--- src/modules/home/presentation/components/Navbar.tsx
+++ src/modules/home/presentation/components/Navbar.tsx
@@ -3,8 +3,8 @@ import { Link } from 'react-router-dom';
 const NAV_LINKS = [
   { label: 'PRINCIPAL', href: '/', active: true },
   { label: 'PREION', href: '#' },
-  { label: 'SERVICES', href: '#' },
-  { label: 'ABOUT', href: '#' },
+  { label: 'SERVICIOS', href: '#' },
+  { label: 'ACERCA DE', href: '#' },
 ];
 
 const PortlyLogo = () => (
