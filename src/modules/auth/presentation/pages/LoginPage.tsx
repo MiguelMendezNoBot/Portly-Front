2c4be8b4635f0aa5bb447f14c96c842c7e84d70c
Daynor Montoya Chura <montoya.chura.daynor@gmail.com>
2026-04-07 23:42:06 -0400

    fix: prevent login form overlap on mobile screens

--- src/modules/auth/presentation/pages/LoginPage.tsx
+++ src/modules/auth/presentation/pages/LoginPage.tsx
@@ -4,10 +4,12 @@ import BotonInicio from '../../../../shared/components/BotonInicio';
 export default function LoginPage() {
   return (
     <div className="min-h-screen bg-white p-2 md:p-4 box-border">
-      <div className="relative w-full min-h-[calc(100vh-1rem)] md:min-h-[calc(100vh-2rem)] bg-[#0f111a] rounded-[2rem] flex items-center justify-center  shadow-2xl">
+      <div className="relative w-full min-h-[calc(100vh-1rem)] md:min-h-[calc(100vh-2rem)] bg-[#0f111a] rounded-[2rem] flex items-center justify-center shadow-2xl">
         <BotonInicio texto="Volver al inicio" />
 
-        <LoginForm />
+        <div className="mt-20 md:mt-0 w-full flex justify-center">
+          <LoginForm />
+        </div>
       </div>
     </div>
   );
