4d780466b7376ff4d0e9de2e8c0f73d06903132a
CrhistoferVera <cristofervera110@gmail.com>
2026-04-22 19:23:00 -0400

    change button name

--- src/modules/professional/presentation/components/trajectory/ProfessionalExperienceSection.tsx
+++ src/modules/professional/presentation/components/trajectory/ProfessionalExperienceSection.tsx
@@ -173,7 +173,7 @@ export default function TrayectoriaSection() {
             onClick={handleOpenAdd}
             className="bg-gradient-to-r from-[#bdbefe] to-[#a092ec] hover:bg-[#5a52d5] text-[#0D0096] py-3 px-8 rounded-full font-semibold transition-all shadow-[0_0_15px_rgba(108,99,255,0.3)] active:scale-95 text-sm"
           >
-            AGREGAR NUEVA EXPERIENCIA
+            AGREGAR EXPERIENCIA
           </button>
         </div>
       </section>
